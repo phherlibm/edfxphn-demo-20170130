5309ca40fa7987e3c9ab88e5917753924d4ece5a
PHILIPPE HERLIN <philippe.herlin@fr.ibm.com>
2017-01-30 12:22:57 -0600

    Utilisation des variables d'environnements Bluemix

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
-/*eslint-env node, express, cfenv, express-session, watson-developer-cloud*/
+/*eslint-env node, express, cfenv, express-session, watson-developer-cloud, body-parser */
 
 //------------------------------------------------------------------------------
 // node.js starter application for Bluemix
 //------------------------------------------------------------------------------
 
+console.log("Démarrage de l'application");
 var express = require('express');
 var cfenv = require('cfenv');
 var session = require("express-session");
 var app = express();
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
+
+// Environnement
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+console.log("service : " + JSON.stringify(appEnv.getService(/translator/i)));
+console.log("service credentials : " + JSON.stringify(appEnv.getService(/translator/i).credentials));
+console.log("service url : " + JSON.stringify(appEnv.getService(/translator/i).credentials.url));
+console.log("service user : " + JSON.stringify(appEnv.getService(/translator/i).credentials.username));
+console.log("service pws : " + JSON.stringify(appEnv.getService(/translator/i).credentials.password));
+
 
 // Watson Services
 var LanguageTranslatorV2 = require("watson-developer-cloud/language-translator/v2");
-var language_translator = new LanguageTranslatorV2({
-  username: "8e7933a9-8cfe-4ff2-9c37-47401933cefe",
-  password: "KtuvlKGmadBF",
-  url: 'https://gateway.watsonplatform.net/language-translator/api/'
-});
+var language_translator;
+var translatorService = appEnv.getService(/translator/i);
+if (translatorService) {
+	console.log("Initialisation du service à partir de Bluemix.");
+	language_translator = new LanguageTranslatorV2({
+  		username: translatorService.credentials.username,
+	  	password: translatorService.credentials.password,
+  		url: translatorService.credentials.url
+	});
+} else {
+	console.log("Initialisation du service à partir d'une exécution locale.");
+	language_translator = new LanguageTranslatorV2({
+  		username: "8e7933a9-8cfe-4ff2-9c37-47401933cefe",
+	  	password: "KtuvlKGmadBF",
+  		url: 'https://gateway.watsonplatform.net/language-translator/api/'
+	});
+}
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -71,13 +95,10 @@ app.use(function(request, response) {
 });
 */
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
 
-console.log("appEnv : " + JSON.stringify(appEnv, null, 2));
+
