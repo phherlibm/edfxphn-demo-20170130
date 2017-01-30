bb00e3bcc180a86fe8789427dc868aaa7858ba99
pherlibm <philippe.herlin@fr.ibm.com>
2017-01-30 18:08:18 +0100

    Dump cloud foundry environment.

--- app.js
+++ app.js
@@ -79,3 +79,5 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+console.log("appEnv : " + JSON.stringify(appEnv, null, 2));
